f04b9ac17c6503c0bbd12bed95776ade9538b06d
Ankita Dodia <dodia.ankita30@gmail.com>
2019-10-19 11:27:18 +0530

    Update pay load parsing

--- app/src/main/java/com/estimotelib/fcm/CustomFirebaseMessagingService.java
+++ app/src/main/java/com/estimotelib/fcm/CustomFirebaseMessagingService.java
@@ -1,17 +1,11 @@
 package com.estimotelib.fcm;
 
-import android.app.ActivityManager;
-import android.content.ComponentName;
-import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v4.content.LocalBroadcastManager;
 import android.util.Log;
-import com.estimotelib.FCMNotificationManager;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import org.json.JSONObject;
-import java.util.List;
 import java.util.Map;
 
 public class CustomFirebaseMessagingService extends FirebaseMessagingService
@@ -40,41 +34,30 @@ public class CustomFirebaseMessagingService extends FirebaseMessagingService
         try {
             JSONObject dataJSON = new JSONObject(data);
             Log.e("onMessageReceived-->","" + data);
-            if(remoteMessage.getNotification().getTitle() != null){
-                processNotification(dataJSON,remoteMessage.getNotification().getTitle(),
-                        remoteMessage.getNotification().getBody());
-            }else {
-                processNotification(dataJSON,"",remoteMessage.getNotification().getBody());
-            }
+            processNotification(dataJSON);
 
         } catch (Exception e) {
             Log.e("onMessageReceived-->","" + e.getMessage());
         }
-
-        if (remoteMessage.getNotification() != null) {
-            Log.e(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
-            Log.e(TAG, "Message Notification Title: " + remoteMessage.getNotification().getTitle());
-        }
     }
 
-    private void processNotification(JSONObject data,String title,String msg)
+    private void processNotification(JSONObject data)
     {
         String Image = data.optString("image");
         String url = data.optString("url");
-        String appName   = data.optString("app_name");
+        String title   = data.optString("title");
+        String msg   = data.optString("message");
 
         Log.e(TAG,"Image: "+Image);
         Log.e(TAG,"url: "+url);
         Log.e(TAG,"title: "+title);
         Log.e(TAG,"message: "+msg);
-        Log.e(TAG,"appName: "+appName);
 
         Intent intent = new Intent("NewNotification");
         intent.putExtra("image", Image);
         intent.putExtra("url", url);
         intent.putExtra("title", title);
         intent.putExtra("message", msg);
-        intent.putExtra("app_name", appName);
         broadcaster.sendBroadcast(intent);
     }
 }
