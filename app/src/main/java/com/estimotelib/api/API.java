d3b97e662057e7d7db50a7ac5304fa7d61729169
Ankita Dodia <dodia.ankita30@gmail.com>
2019-11-21 19:33:11 +0530

    Update base url

--- app/src/main/java/com/estimotelib/api/API.java
+++ app/src/main/java/com/estimotelib/api/API.java
@@ -2,7 +2,7 @@ package com.estimotelib.api;
 
 public interface API {
 //    String BASE_URL = "http://mobappstesting.website/Beaconnotification/public/api/";
-    String BASE_URL = "http://push.iotmarketingmedia.com/api";
+    String BASE_URL = "http://push.iotmarketingmedia.com/api/";
 
     String VISIT_PROPERTY = "notification/property-visit";
     String EXIT_PROPERTY = "notification/property-exit";
